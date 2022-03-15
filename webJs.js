b34309706457e5104b84e368e18d13074835d4d7
SampathBojja <100344164+SampathBojja@users.noreply.github.com>
2022-03-15 19:03:47 +0530

    Add files via upload

---
+++ webJs.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".about, .BENCHMARKING,.TRANSFORMATION,.STUDIO,.SERVICES,.PLATFORMS").on({
+        mouseenter: function(){$(this).css("font-size","medium")},
+        mouseleave: function(){$(this).css("font-size","small")},
+})
+})
